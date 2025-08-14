2426cb21e0816e30e43612072fc800480fd231c0
myeng05 <myeng0525@naver.com>
2025-08-15 03:05:59 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -515,7 +515,7 @@ def render_financial_results():
         )
     
     with tab2:
-        st.markdown("**💵 고정비**")
+        st.markdown("**🏢 고정비 분석**")
         # 고정비 관련 항목들만 필터링 (계산된 고정비 포함)
         fixed_items = ['고정비', '감가상각비', '인건비', '임차료', '관리비', '고정비율(%)']
         fixed_df = final_df[final_df['구분'].isin(fixed_items) | final_df['구분'].str.startswith('  └')]
